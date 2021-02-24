7d287343249f1a9ee68533010c8b320c5b12af7b
NemisIGuess <ismaelpjgg@gmail.com>
2021-02-24 12:08:25 +0100

    Finished the Ship class and its tests

--- src/components/shipFactory.js
+++ src/components/shipFactory.js
@@ -1,93 +1,66 @@
-const shipFactory = (name) => {
-  class Carrier {
+const shipFactory = (name, length) => {
+  class Ship {
     constructor() {
-      this.name = 'carrier';
-      this.hits = [];
+      this.name = name;
+      this.hits = length;
       this.position = [];
-      this.length = 5;
+      this.length = length;
       this.axis = 'x';
       this.isSunk = false;
     }
 
-    isHit(index) {
-      this.hits.push(index);
+    setPosition(index) {
+      switch (this.axis) {
+        case 'y':
+          // Comprueba si la posicion es valida en el eje Y
+          if (index + (this.length - 1) * 10 > 99) {
+            return;
+          }
+          // Mete la posicion dentro del array position para el eje Y
+          for (let i = 0; i < this.length; i++) {
+            this.position.push(index + 10 * i);
+          }
+          break;
+        case 'x':
+          // Comprueba si la posicion es valida en el eje X
+          if (
+            (index < 9 && index + this.length - 1 > 9) ||
+            (index > 9 && index < 19 && index + this.length - 1 > 19) ||
+            (index > 19 && index < 29 && index + this.length - 1 > 29) ||
+            (index > 29 && index < 39 && index + this.length - 1 > 39) ||
+            (index > 39 && index < 49 && index + this.length - 1 > 49) ||
+            (index > 49 && index < 59 && index + this.length - 1 > 59) ||
+            (index > 59 && index < 69 && index + this.length - 1 > 69) ||
+            (index > 69 && index < 79 && index + this.length - 1 > 79) ||
+            (index > 79 && index < 89 && index + this.length - 1 > 89) ||
+            (index > 89 && index < 99 && index + this.length - 1 > 99)
+          ) {
+            return;
+          }
+          // Mete la posicion dentro del array position para el eje X
+          for (let i = 0; i < this.length; i++) {
+            this.position.push(index + i);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
+    changeAxis() {
+      this.axis === 'x' ? (this.axis = 'y') : (this.axis = 'x');
+    }
+
+    isHit() {
+      this.hits = this.hits - 1;
+      // Comprueba si ha sido golpeada suficientes veces y si es asi se hunde
+      if (this.hits === 0) {
+        this.isSunk = true;
+      }
     }
   }
 
-  class Battleship {
-    constructor() {
-      this.name = 'battleship';
-      this.hits = [];
-      this.position = [];
-      this.length = 4;
-      this.axis = 'x';
-      this.isSunk = false;
-    }
-
-    isHit(index) {
-      this.hits.push(index);
-    }
-  }
-
-  class Destroyer {
-    constructor() {
-      this.name = 'destroyer';
-      this.hits = [];
-      this.position = [];
-      this.length = 3;
-      this.axis = 'x';
-      this.isSunk = false;
-    }
-
-    isHit(index) {
-      this.hits.push(index);
-    }
-  }
-
-  class PatrolBoat {
-    constructor() {
-      this.name = 'patrol boat';
-      this.hits = [];
-      this.position = [];
-      this.length = 2;
-      this.axis = 'x';
-      this.isSunk = false;
-    }
-
-    isHit(index) {
-      this.hits.push(index);
-    }
-  }
-
-  class FishingBoat {
-    constructor() {
-      this.name = 'fishing boat';
-      this.hits = [];
-      this.position = [];
-      this.length = 1;
-      this.axis = 'x';
-      this.isSunk = false;
-    }
-
-    isHit(index) {
-      this.hits.push(index);
-    }
-  }
-
-  switch (name) {
-    case 'carrier':
-      return new Carrier();
-    case 'battleship':
-      return new Battleship();
-    case 'destroyer':
-      return new Destroyer();
-    case 'patrol boat':
-      return new PatrolBoat();
-    case 'fishing boat':
-      return new FishingBoat();
-    default:
-      break;
-  }
+  return new Ship();
 };
 
 export default shipFactory;
--- src/tests/shipFactory.test.js
+++ src/tests/shipFactory.test.js
@@ -3,9 +3,9 @@ import shipFactory from '../components/shipFactory';
 
 describe('Creates ships depending on what is asked', () => {
   test('returns a Carrier', () => {
-    expect(shipFactory('carrier')).toEqual({
+    expect(shipFactory('carrier', 5)).toEqual({
       name: 'carrier',
-      hits: [],
+      hits: 5,
       position: [],
       length: 5,
       axis: 'x',
@@ -13,9 +13,9 @@ describe('Creates ships depending on what is asked', () => {
     });
   });
   test('returns a Battleship', () => {
-    expect(shipFactory('battleship')).toEqual({
+    expect(shipFactory('battleship', 4)).toEqual({
       name: 'battleship',
-      hits: [],
+      hits: 4,
       position: [],
       length: 4,
       axis: 'x',
@@ -23,9 +23,9 @@ describe('Creates ships depending on what is asked', () => {
     });
   });
   test('returns a Destroyer', () => {
-    expect(shipFactory('destroyer')).toEqual({
+    expect(shipFactory('destroyer', 3)).toEqual({
       name: 'destroyer',
-      hits: [],
+      hits: 3,
       position: [],
       length: 3,
       axis: 'x',
@@ -33,34 +33,124 @@ describe('Creates ships depending on what is asked', () => {
     });
   });
   test('returns a Patrol Boat', () => {
-    expect(shipFactory('patrol boat')).toEqual({
+    expect(shipFactory('patrol boat', 2)).toEqual({
       name: 'patrol boat',
-      hits: [],
+      hits: 2,
       position: [],
       length: 2,
       axis: 'x',
       isSunk: false,
     });
   });
-  test('returns a Fishing Boat', () => {
-    expect(shipFactory('fishing boat')).toEqual({
-      name: 'fishing boat',
-      hits: [],
+});
+
+describe('ship methods work', () => {
+  let ship1;
+  beforeEach(() => {
+    ship1 = shipFactory('destroyer', 3);
+  });
+
+  test('ship has been hit', () => {
+    ship1.isHit();
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 2,
       position: [],
-      length: 1,
+      length: 3,
       axis: 'x',
       isSunk: false,
     });
   });
+
+  test('ship has been set on axis Y', () => {
+    ship1.changeAxis();
+    ship1.setPosition(13);
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 3,
+      position: [13, 23, 33],
+      length: 3,
+      axis: 'y',
+      isSunk: false,
+    });
+  });
+
+  test('ship wont set if is out of bounds on axis Y', () => {
+    ship1.changeAxis();
+    ship1.setPosition(83);
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 3,
+      position: [],
+      length: 3,
+      axis: 'y',
+      isSunk: false,
+    });
+  });
+
+  test('ship has been set on axis X', () => {
+    ship1.setPosition(52);
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 3,
+      position: [52, 53, 54],
+      length: 3,
+      axis: 'x',
+      isSunk: false,
+    });
+  });
+
+  test('ship wont set if is out of bounds on axis X', () => {
+    ship1.setPosition(78);
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 3,
+      position: [],
+      length: 3,
+      axis: 'x',
+      isSunk: false,
+    });
+  });
+
+  test('ship changes axis', () => {
+    ship1.changeAxis();
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 3,
+      position: [],
+      length: 3,
+      axis: 'y',
+      isSunk: false,
+    });
+  });
+
+  test('ship has been sunk', () => {
+    ship1.setPosition(13);
+    ship1.isHit();
+    ship1.isHit();
+    ship1.isHit();
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 0,
+      position: [13, 14, 15],
+      length: 3,
+      axis: 'x',
+      isSunk: true,
+    });
+  });
+
+  test('ship sinks even if hits are out of order', () => {
+    ship1.setPosition(13);
+    ship1.isHit();
+    ship1.isHit();
+    ship1.isHit();
+    expect(ship1).toEqual({
+      name: 'destroyer',
+      hits: 0,
+      position: [13, 14, 15],
+      length: 3,
+      axis: 'x',
+      isSunk: true,
+    });
+  });
 });
-
-// describe('Ships can be hit', () => {
-//   let ship1;
-//   beforeEach(() => {
-//     ship1 = shipFactory('carrier');
-//   });
-
-//   test('can recieve a hit and store it', () => {
-//     expect(s)
-//   })
-// });
